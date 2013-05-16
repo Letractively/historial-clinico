99dd46dc51cc4543c66b471b55cf7d6c59e6c32e
OIRADA <OIRADA@gmail.com>
2013-05-16 02:19:04 +0000

    Subido 9_

---
+++ Proyecto/hospital_0.7/src/java/com/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+
+import com.modelo.Usuario;
+
+/**
+ *
+ * @author Oscar Rada
+ */
+public interface UsuarioDao {
+    
+    public Usuario buscarPorUsuario(Usuario usuario);
+    
+}
