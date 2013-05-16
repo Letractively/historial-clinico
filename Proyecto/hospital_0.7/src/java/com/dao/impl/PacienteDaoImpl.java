3c03ce6e5a2e443b0b4353e232eb43b0d00f085a
OIRADA <OIRADA@gmail.com>
2013-05-16 02:18:01 +0000

    Subido 9_

---
+++ Proyecto/hospital_0.7/src/java/com/dao/impl/PacienteDaoImpl.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao.impl;
+
+import com.dao.PacienteDao;
+import com.modelo.Paciente;
+import com.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Oscar Rada
+ */
+public class PacienteDaoImpl implements PacienteDao{
+
+    @Override
+    public void insertar(Paciente paciente) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.save(paciente);
+            session.beginTransaction().commit();
+        }catch (Exception e){
+            System.out.println("Error en insertar: "+e.getMessage());
+            session.beginTransaction().rollback();
+        }
+    }
+
+    @Override
+    public void actualizar(Paciente paciente) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.update(paciente);
+            session.beginTransaction().commit();
+        }catch (Exception e){
+            System.out.println("Error en actualizar: "+e.getMessage());
+            session.beginTransaction().rollback();
+        }
+    }
+
+    @Override
+    public void deshabilitar(Paciente paciente) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.update(paciente);
+            session.beginTransaction().commit();
+        }catch (Exception e){
+            System.out.println("Error en deshabilitar: "+e.getMessage());
+            session.beginTransaction().rollback();
+        }
+    }
+
+    @Override
+    public Paciente buscarPorId(Integer id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        return (Paciente) session.load(Paciente.class, id);
+    }
+
+    @Override
+    public List<Paciente> buscarTodos() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        return session.createQuery("from Paciente").list();
+    }
+    
+}
---
+++ Proyecto/hospital_0.7/src/java/com/dao/impl/UsuarioDaoImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao.impl;
+
+import com.dao.UsuarioDao;
+import com.modelo.Usuario;
+import com.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Oscar Rada
+ */
+public class UsuarioDaoImpl implements UsuarioDao{
+
+    @Override
+    public Usuario buscarPorUsuario(Usuario usuario) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String sql;
+        sql = "select u from Usuario u "
+                + "where login = :user "
+                + "and clave = md5(:pass)";
+        Query query = session.createQuery(sql);
+        query.setString("user", usuario.getLogin());
+        query.setString("pass", usuario.getClave());
+        return (Usuario) query.uniqueResult();
+    }
+    
+}
